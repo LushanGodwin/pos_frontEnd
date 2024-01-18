a69945e63cc1050433685f517969fff0277b83e4
Your Name <you@example.com>
2024-01-18 21:23:32 +0530

    add order

---
+++ api/orderAPI.js
@@ -0,0 +1,24 @@
+export class OrderAPI {
+
+    generateOderID() {
+        return new Promise((resolve, reject)=>{
+
+            const http = new XMLHttpRequest();
+
+            http.onreadystatechange = ()=>{
+                if (http.readyState===4){
+                    if(http.status===200){
+                        resolve(http.responseText);
+                    }else {
+                        console.error('Error:', http.status, http.statusText);
+                        reject(new Error(`HTTP request failed with status ${http.status}`));
+                    }
+                }
+            }
+
+            http.open("GET", "http://localhost:8080/page/Oder?action=generateOderID", true);
+            http.send();
+        });
+    }
+
+}
